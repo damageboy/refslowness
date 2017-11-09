0f12d2f60f98fb0673066856612f89a68a4b7ee6
Dan Shechter <dans@houmus.org>
2017-11-09 11:07:47 +0200

    bah

---
+++ CircularBufferRef.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace xxx
+{
+  public class 	CircularBufferRef<T> : IEnumerable<T> where T : struct
+  {
+    readonly T[] _buffer;
+    int _head;
+
+    public CircularBufferRef(int capacity)
+    {
+      if (capacity < 0)
+        throw new ArgumentOutOfRangeException(nameof(capacity), "must be positive");
+      _buffer = new T[capacity];
+      _head = 0;
+      Capacity = capacity;
+      Enqueue = InitialEnqueue;
+    }
+
+    public int Count { get; private set; }
+
+    public int Capacity { get; }
+
+    public delegate T AddDelegate(T v);
+
+    public T Last { get; private set; }
+
+    /// <summary>
+    /// Adds the specified input value from the data structure.
+    /// </summary>
+    /// <param name="d">The value to add</param>
+    public AddDelegate Enqueue { get; private set; }
+
+    public unsafe T InitialEnqueue(T d)
+    {
+      Count++;
+      _buffer[_head] = Last = d;
+
+      ItemAdded?.Invoke(this, d);
+
+      if (_head < Capacity)
+        return d;
+
+      // The sliding window is full, change behaviour from now on
+      Enqueue = CircularEnqueue;
+      _head = 0;
+      return d;
+    }
+
+    public delegate void NewValue(CircularBufferRef<T> buf, T value);
+
+    public event NewValue ItemAdded;
+
+    public unsafe T CircularEnqueue(T d)
+    {
+      _buffer[_head] = Last = d;
+
+      if (_head == Capacity)
+        _head = 0;
+
+      ItemAdded?.Invoke(this, d);
+
+      return d;
+    }
+
+    public void Clear()
+    {
+      _head = 0;
+      Count = 0;
+    }
+
+    public ref T this[int index]
+    {
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      get
+      {
+#if DEBUG
+        if (index < 0 || index >= Count)
+          throw new ArgumentOutOfRangeException(nameof(index), $"Bad index {index} was supplied, Count is {Count}");
+        if (Count != Capacity)
+          throw new InvalidOperationException("The indexer may not be called before the buffer is full");
+#endif
+
+        return ref _buffer[(_head + index) % Capacity];
+      }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      if (Count == 0 || Capacity == 0)
+        yield break;
+
+      for (var i = 0; i < Count; ++i)
+        yield return this[i];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace xxx
+{
+    public class Config : ManualConfig
+    {
+        public Config()
+        {
+            Add(new Job()
+            {
+                Env = { Runtime = Runtime.Clr, Jit = Jit.RyuJit , Platform = Platform.X64 },
+                Run = { LaunchCount = 1, WarmupCount = 1, TargetCount = 10 },
+            });
+            //Add(Job.Mono);
+            Add(StatisticColumn.Min);
+            Add(StatisticColumn.Max);
+            //Add(new MemoryDiagnoser());
+            Add(RPlotExporter.Default);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var competition = new BenchmarkSwitcher(new[] {
+                typeof(CircularBufferBenchmarks),
+                typeof(CircularBufferRefBenchmarks),
+            });
+
+            competition.Run(args);
+        }
+    }
+
+    
+    [Config(typeof(Config))]
+    public class CircularBufferBenchmarks
+    {
+        CircularBuffer<double> _cb;
+        [GlobalSetup]
+        public void Setup()
+        {
+            _cb = new CircularBuffer<double>(1024);
+            foreach (var d in Enumerable.Range(0, 1024))
+                _cb.Enqueue(d);
+        }
+
+        [Benchmark(OperationsPerInvoke = 10)]
+        public double Indexer()
+        {
+            double x = 0 ;
+            var c = _cb.Count - 1;
+            for (var i = 0; i < 10; i++)
+              x += _cb[c];
+            return x;
+        }
+    }
+    
+    [Config(typeof(Config))]
+    public class CircularBufferRefBenchmarks
+    {
+        CircularBufferRef<double> _cb;
+        [GlobalSetup]
+        public void Setup()
+        {
+            _cb = new CircularBufferRef<double>(1024);
+            foreach (var d in Enumerable.Range(0, 1024))
+                _cb.Enqueue(d);
+        }
+
+        [Benchmark(OperationsPerInvoke = 10)]
+        public double Indexer()
+        {
+            double x = 0 ;
+            var c = _cb.Count - 1;
+            for (var i = 0; i < 10; i++)
+                x += _cb[c];
+            return x;
+        }
+    }
+}
+
